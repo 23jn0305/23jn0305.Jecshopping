228bd0ca4829c9715210f26da8fa9b286551d61e
23jn0305 <23jn0305@jec.ac.jp>
2024-12-18 11:27:02 +0900

    Delete JecShopping(完成版) directory

--- JecShopping(完成版)/header2.php
+++
@@ -1,8 +0,0 @@
-<header>
-   <div>
-      <a href="index.php">
-         <img src="images/JecShoppingLogo.jpg" alt="JecShoppingロゴ" />
-      </a>
-   </div>
-   <hr>
-</header>
--- JecShopping(完成版)/helpers/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-    // DB接続設定
-    define('DSN', 'sqlsrv:server=10.32.97.1;database=20jn0300');
-    define('DB_USER', '20jn0300');
-    define('DB_PASSWORD', '20jn0300');
