b0e00d0657339508954cd9c04c34dc49ca436804
23jn0305 <23jn0305@jec.ac.jp>
2024-12-25 11:31:23 +0900

    db setting

--- helpers/config.php
+++ helpers/config.php
@@ -1,5 +1,5 @@
 <?php
     // DB接続設定
-    define('DSN', 'sqlsrv:server=10.32.97.1;database=20jn0300');
-    define('DB_USER', '20jn0300');
-    define('DB_PASSWORD', '20jn0300');
+    define('DSN', 'sqlsrv:server = tcp:23jn0305db.database.windows.net,1433; Database = 23jn0305DB');
+    define('DB_USER', 'jndb');
+    define('DB_PASSWORD', 'Pa$$word1234');
